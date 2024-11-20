782dc0ab6f0bf3700e5d74fb4a27179f2320c0e2
maxfa5 <maksfa04@mail.ru>
2024-11-20 22:28:21 +0500

    add slog

--- src/Consumer/main.go
+++ src/Consumer/main.go
@@ -1,45 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
-	"log"
+	"log/slog"
+	"os"
 )
 
 func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	// Укажите адреса брокеров
 	brokers := "localhost:9092,localhost:9093,localhost:9091"
 	topic := "orders"
-	group_id := "test_group"
+	groupId := "test_group"
 	// Проверка подключения
-	consumer, err := connToKafka(brokers, topic, group_id)
+	consumer, err := connToKafka(brokers, topic, groupId)
 	if err != nil {
-		log.Fatalf("Ошибка подключения к Kafka: %v", err)
+		logger.Error("Ошибка подключения к Kafka: %v", err)
 	}
 	defer consumer.Close()
 
-	fmt.Println("Consumer успешно подключен к Kafka. Ожидание сообщений...")
+	logger.Info("Consumer успешно подключен к Kafka. Ожидание сообщений...")
 
-	// Получение сообщений (можно просто ждать, чтобы проверить подключение)
 	for {
 		msg, err := consumer.ReadMessage(-1)
 		if err != nil {
 			// Если произошла ошибка, выводим ее
-			fmt.Printf("Ошибка при чтении сообщения: %v\n", err)
+			logger.Error("Ошибка при чтении сообщения: %v\n", err)
 			break
 		}
 		// Если сообщение получено, выводим его
-		fmt.Printf("Получено сообщение: %s\n", string(msg.Value))
+		logger.Info("Получено сообщение: %s\n", string(msg.Value))
 	}
 
 	// Закрываем Consumer
 	consumer.Close()
+	logger.Info("Программа заверена")
 }
 
-func connToKafka(brokers string, topic string, group_id string) (c *kafka.Consumer, err error) {
+func connToKafka(brokers string, topic string, groupId string) (c *kafka.Consumer, err error) {
 	c, err = kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": brokers,
-		"group.id":          group_id,
+		"group.id":          groupId,
 		"auto.offset.reset": "earliest",
 	})
 	if err != nil {
