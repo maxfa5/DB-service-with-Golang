ee9d477dc8893a79bdedb8d5075026eb31175e8e
maxfa5 <maksfa04@mail.ru>
2025-01-25 21:36:23 +0500

    fix consumer add logs + fix healthcheck in docker-compose

--- src/Consumer/go.mod
+++ src/Consumer/go.mod
@@ -1,5 +1,8 @@
 module Consumer
 
-go 1.23.3
+go 1.23.5
 
-require github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
+require (
+	github.com/confluentinc/confluent-kafka-go v1.9.2 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- src/Consumer/main.go
+++ src/Consumer/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"context"
+	"fmt"
 	"log/slog"
 	"os"
+	"time"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 func main() {
@@ -14,26 +18,17 @@ func main() {
 	groupId := "test_group"
 	// Проверка подключения
 	consumer, err := connToKafka(brokers, topic, groupId)
-	if err != nil {
-		logger.Error("Ошибка подключения к Kafka: %v", err)
-	}
 	defer consumer.Close()
-
-	logger.Info("Consumer успешно подключен к Kafka. Ожидание сообщений...")
-
-	for {
-		msg, err := consumer.ReadMessage(-1)
-		if err != nil {
-			// Если произошла ошибка, выводим ее
-			logger.Error("Ошибка при чтении сообщения: %v\n", err)
-			break
-		}
-		// Если сообщение получено, выводим его
-		logger.Info("Получено сообщение: %s\n", string(msg.Value))
+	if err != nil {
+		logger.Error("Ошибка подключения к Kafka", slog.String("error", err.Error()))
+	} else {
+		logger.Info("Consumer успешно подключен к Kafka. Ожидание сообщений...")
+	}
+	msg, err := loopCathMsg(logger, consumer, 10000*time.Millisecond)
+	if err == nil {
+		logger.Info("Получено сообщение:", slog.Any("", string(msg.Value)))
 	}
 
-	// Закрываем Consumer
-	consumer.Close()
 	logger.Info("Программа заверена")
 }
 
@@ -55,3 +50,35 @@ func connToKafka(brokers string, topic string, groupId string) (c *kafka.Consume
 	}
 	return c, nil
 }
+
+func loopCathMsg(logger *slog.Logger, consumer *kafka.Consumer, timeout time.Duration) (msg *kafka.Message, err error) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	for {
+		select {
+		case <-ctx.Done():
+			err := fmt.Errorf("timeout while waiting for message: %w", ctx.Err())
+			logger.Warn("Timeout while waiting for message", slog.String("error", err.Error()))
+			return nil, err
+		default:
+			msg, err := consumer.ReadMessage(100 * time.Millisecond) // Non-blocking call with a timeout
+			if err != nil {
+				if kafkaErr, ok := err.(kafka.Error); ok && kafkaErr.Code() == kafka.ErrTimedOut {
+					logger.Debug("No message received yet, continuing loop", slog.String("error", err.Error()))
+					continue
+				}
+				err = fmt.Errorf("error while reading from kafka: %w", err)
+				logger.Error("Error while reading from kafka", slog.String("error", err.Error())) //Логируем на уровне где это имеет смысл
+				return nil, err
+			}
+			// Если сообщение получено, возвращаем его
+			logger.Debug("Message received", slog.Any("key", msg.Key), slog.Any("headers", msg.Headers))
+			return msg, nil
+		}
+
+	}
+}
+
+func connToPosgre() {}
+
+func SendMessToDB() {}
